6d31c857b143b500e0fcf1e3a86e4c59e21825e3
Korobenko2023 <vivika3110@gmail.com>
2023-08-30 23:21:30 +0300

    task-1

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,51 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
+  const container = document.querySelector('.gallery');
+  const markup = createMarkup(galleryItems)
+  
+  container.insertAdjacentHTML('beforeend', markup);
+  container.addEventListener('click', handleGalleryItemsClick);
+  let instance = null;
 
-console.log(galleryItems);
+    function createMarkup (arr) {
+    return arr.map(({ original, preview, description }) => {
+      return `<li class="gallery__item">
+      <a class="gallery__link" href="${original}">
+      <img
+        class="gallery__image"
+        src="${preview}"
+        data-source="${original}"
+        alt="${description}"
+      />
+    </a>
+      </li>`
+    }).join('');
+  };
+
+  function handleGalleryItemsClick (event) {
+    event.preventDefault();
+
+    if (event.target === event.currentTarget) {
+      return;
+    }
+
+    const targetElement = event.target.closest('.gallery__image')
+    const largeImage = targetElement.dataset.source;
+
+    instance = basicLightbox.create(`
+    <div class="modal">
+    <img src="${largeImage}" alt="" />
+    </div>
+   `);
+
+   instance.show();
+
+   window.addEventListener('keydown', handleKeyPress);
+   }
+
+   function handleKeyPress(event) {
+    if (event.key === 'Escape') {
+      instance.close();
+      window.removeEventListener('keydown', handleKeyPress);
+    }
+   }
