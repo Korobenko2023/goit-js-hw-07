0303f68bbdd556f28a115d0bf194795c3e4584be
Korobenko2023 <vivika3110@gmail.com>
2023-09-01 01:08:00 +0300

    task-2

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,19 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
+  const container = document.querySelector('.gallery');
+  const markup = createMarkup(galleryItems);
+  container.insertAdjacentHTML('beforeend', markup);
+  const lightbox = new SimpleLightbox('.gallery a', {
+    captionsData: 'alt',
+    captionDelay: 250
+   });
 
-console.log(galleryItems);
+    function createMarkup (arr) {
+    return arr.map(({ original, preview, description }) => {
+      return `<li class="gallery__item">
+      <a class="gallery__link" href="${original}" >
+         <img class="gallery__image" src="${preview}" alt="${description}" />
+     </a>
+   </li>`
+    }).join('');
+  };
